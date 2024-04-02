d37bd7742c6d98a3b34eb96b283dceccf4ba2be5
Walter Teitelbaum <walter@teitelbaum.us>
2024-04-01 19:10:54 -0700

    Update users_controller.rb

--- controllers/users_controller.rb
+++ controllers/users_controller.rb
@@ -5,3 +5,42 @@ Date: 4/1/2024
 Modified: 4/1/2024
 
 Description: This file will handle CRUD operations for users.
+"""
+
+require 'sinatra/base'
+require './models/user'
+
+class UsersController < Sinatra::Base
+  # Create
+  post '/users' do
+    @user = User.new(params[:user])
+    if @user.save
+      redirect to("/users/#{@user.id}")
+    else
+      # TODO: handle error
+    end
+  end
+
+  # Read
+  get '/users/:id' do
+    @user = User.find(params[:id])
+    # TODO: render game stats view
+  end
+
+  # Update
+  put '/users/:id' do
+    @user = User.find(params[:id])
+    if @user.update(params[:user])
+      redirect to("/users/#{@user.id}")
+    else
+      # TODO: handle error
+    end
+  end
+
+  # Delete
+  delete '/users/:id' do
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect to('/users')
+  end
+end
