c0d7cb5c5a354e786f2015eee1041630b90b70eb
Walter Teitelbaum <walter@teitelbaum.us>
2024-04-01 19:36:28 -0700

    Update app.rb

--- app.rb
+++ app.rb
@@ -20,13 +20,47 @@ class App < Sinatra::Base
     if @game
       @game.to_json
     else
-      # TODO: handle error
+      status 404
+      "Error: No game found for today's date."
     end
   end
 
   # GET /stats
   get '/stats' do
-    @users = User.all
-    @users.to_json
+    uuid = request.cookies['user_uuid']
+    @user = User.find_by(uuid: uuid)
+    if @user
+      @user.to_json
+    else
+      status 404
+      "Error: No user found with the provided UUID."
+    end
   end
+
+  # PUT /soln
+  put '/soln' do
+    uuid = request.cookies['user_uuid']
+    @user = User.find_by(uuid: uuid)
+    @game = Game.find_by(game_number: params[:game_id])
+
+    if @game && @game.date == Date.today
+      @solution = Solution.find_by(user_id: @user.id, game_id: @game.id)
+
+      if @solution
+        if @solution.update(params[:solution])
+          @solution.to_json
+        else
+          status 400
+          "Error: Failed to update the solution."
+        end
+      else
+        status 404
+        "Error: No solution found for the provided user and game."
+      end
+    else
+      status 404
+      "Error: No game found with the provided game number, or the game is not for today's date."
+    end
+  end
+
 end
