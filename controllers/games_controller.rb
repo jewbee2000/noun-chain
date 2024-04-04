4204dd67c74541e0dac1d30631465512ba5e17d6
Benjamin Teitelbaum <ben@Benjamins-MacBook-Pro.local>
2024-04-03 20:18:01 -0700

    Don't want. Don't need.

--- controllers/games_controller.rb
+++
@@ -1,7 +0,0 @@
-"""
-Author: Walter Teitelbaum
-File: games_controller.rb
-Date: 4/1/2024
-Modified: 4/1/2024
-
-Description: This file will handle CRUD operations for games.
--- controllers/solutions_controller.rb
+++
@@ -1,7 +0,0 @@
-"""
-Author: Walter Teitelbaum
-File: solutions_controller.rb
-Date: 4/1/2024
-Modified: 4/1/2024
-
-Description: This file will handle CRUD operations for solutions.
