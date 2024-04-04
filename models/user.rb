e448989e000d5c39e1cae6a261ccc5854e02681c
Benjamin Teitelbaum <ben@Benjamins-MacBook-Pro.local>
2024-04-03 20:23:40 -0700

    This is enough. Collisions are theoretically possible, but in practice are never going to happen. See: https://en.wikipedia.org/wiki/Universally_unique_identifier#Collisions

--- models/user.rb
+++ models/user.rb
@@ -13,7 +13,7 @@ require 'securerandom'
 class User < ActiveRecord::Base
   has_many :solutions
 
-  attribute :uuid, :string, default: -> { assign_uuid(5) }
+  attribute :uuid, :string, default: -> { SecureRandom.uuid }
   attribute :won, :integer, default: 0
   attribute :current_streak, :integer, default: 0
   attribute :max_streak, :integer, default: 0
@@ -24,12 +24,4 @@ class User < ActiveRecord::Base
   attribute :plus_5, :integer, default: 0
   attribute :plus_more, :integer, default: 0
 
-  def self.assign_uuid(length)
-    # try twice, then increase length and recurse
-    2.times do
-      candidate = SecureRandom.alphanumeric(length)
-      return candidate unless User.find_by_uuid(candidate)
-    end
-    assign_uuid(length + 1)
-  end
 end
