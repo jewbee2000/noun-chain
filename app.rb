7b1dc6d61674fa04d7e0cc9e17b3991d2e60efdf
Walter Teitelbaum <walter@teitelbaum.us>
2024-04-12 17:41:16 -0700

    Update user stats upon submitting soln

--- app.rb
+++ app.rb
@@ -8,7 +8,6 @@ Description: This file contains the backend for the wordchain game.
 """
 
 require 'sinatra/base'
-require 'sinatra/reloader' if development?
 require './models/game'
 require './models/user'
 require './models/solution'
@@ -37,36 +36,24 @@ class App < Sinatra::Base
       }.to_json
     end
 
+    # Returns the compound noun of two normalized nouns
+    def normalized_compound(n1, n2)
+      "#{n1} #{n2}".downcase
+    end
+
     def valid_soln(game, noun_array)
       # Initialize Redis
       redis = Redis.new(host: "127.0.0.1", port: 6379)
 
-      # # Check Redis connection
-      # begin
-      #   pong = redis.ping
-      #   puts "Redis connection successful: #{pong}"
-      # rescue Redis::CannotConnectError
-      #   puts "Cannot connect to Redis"
-      #   return false
-      # end
-
       # Check each compound noun in the array
       noun_array.each_cons(2) do |first_noun, second_noun|
-        compound_noun = "#{first_noun} #{second_noun}".downcase
-        # puts "Checking compound noun: #{compound_noun}"
+        compound_noun = normalized_compound(first_noun, second_noun)
         # If the compound noun does not exist in Redis, return false
-        compound_noun_value = redis.get(compound_noun)
-        # puts "Value of #{compound_noun} in Redis: #{compound_noun_value}"
-        if compound_noun_value.nil?
-          # puts "Compound noun #{compound_noun} does not exist in Redis"
+        unless redis.exists?(compound_noun)
           return false
-        else
-          # puts "Compound noun #{compound_noun} exists in Redis"
         end
       end
-
       # If all compound nouns exist in Redis, return true
-      # puts "All compound nouns exist in Redis"
       true
     end
   end
@@ -105,15 +92,32 @@ class App < Sinatra::Base
   # POST /soln
   post '/soln' do
     data = JSON.parse(request.body.read)
-    puts "Received data: #{data}"
-    @game = Game.find_by(date: Date.today) # today's game
-    if data['d'].is_a?(Array) && valid_soln(@game, data['d'])
-      # TODO increment the user's stats
+    @game = Game.find_by(date: Date.today)
+    # check if the solution is valid and that the first and last words are the start and end words
+    if data['d'].is_a?(Array) && valid_soln(@game, data['d']) && data['d'].first == @game.start_word && data['d'].last == @game.end_word
+      # increment the user's stats based on how long their solution is compared to the shortest path of today's game
+      shortest_path_length = @game['shortest_path']
+      user_solution_length = data['d'].length
+      difference = user_solution_length - shortest_path_length
+
+      if difference == 0
+        @user.won += 1
+        @user.current_streak += 1
+        @user.max_streak = [@user.max_streak, @user.current_streak].max
+      elsif difference.between?(1, 5)
+        @user["plus_#{difference}"] += 1
+        @user.current_streak = 0
+      else
+        @user.plus_more += 1
+      end
+      @user.save
       success_response(@user)
     else
-      # puts "Data: #{data}"
+      @user.current_streak = 0
+      @user.save
       fail_response({soln: 'invalid solution'})
     end
+
   end
 
 end
